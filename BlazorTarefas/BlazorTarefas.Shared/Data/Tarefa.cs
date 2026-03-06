87d139c633fb0108d6365bef45e223d02b4bf02e
Filipe Silva <filipe.silva@fdecimal.com>
2026-03-06 17:34:08 +0000

    adding blazor projects

---
+++ BlazorTarefas/BlazorTarefas.Shared/Data/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorTarefas.Shared.Data;
+
+public class Tarefa
+{
+    public Guid ID { get; set; } = Guid.NewGuid();
+    public string? Descricao { get; set; }
+    public bool Concluida { get; set; }
+    public DateTime DataCriacao { get; set; }
+}
---
+++ BlazorTarefas/BlazorTarefas/BlazorTarefas/Program.cs
@@ -0,0 +1,32 @@
+using BlazorTarefas.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorTarefas.Client._Imports).Assembly);
+
+app.Run();
